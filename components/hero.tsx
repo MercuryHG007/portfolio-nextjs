4e3c129363c036d67a4ca1ae9ac46beaefed99eb
Harshit Garg <ggharshit1@gmail.com>
2023-07-26 22:41:14 +0530

    Hero updated

--- components/hero.tsx
+++ components/hero.tsx
@@ -20,82 +20,82 @@ const Hero = () => {
     })
 
     return (
-        <Container>
-            <div
-                className='h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden'
-            >
-                <BackgroundCircles />
-                <Image
-                    className='relative bg-[#C2D84B] rounded-full m-0 object-cover'
-                    alt=''
-                    src="/dp1.png"
-                    width="160"
-                    height="160"
 
-                />
-                <div
-                    className='flex flex-col items-center justify-center z-20'
+        <div
+            className='h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden'
+        >
+            <BackgroundCircles />
+            <Image
+                className='relative bg-[#C2D84B] rounded-full m-0 object-cover'
+                alt=''
+                src="/dp1.png"
+                width="160"
+                height="160"
+
+            />
+            <div
+                className='flex flex-col items-center justify-center z-20'
+            >
+                <h2
+                    className='text-sm uppercase text-neutral-300 pb-2 tracking-[10px] md:tracking-[15px]'
                 >
-                    <h2
-                        className='text-sm uppercase text-neutral-300 pb-2 tracking-[10px] md:tracking-[15px]'
+                    Software Engineer
+                </h2>
+                <h1
+                    className='text-5xl lg:text-6xl font-semibold px-10 '
+                >
+                    <span
+                        className='mr-3'
                     >
-                        Software Engineer
-                    </h2>
-                    <h1
-                        className='text-5xl lg:text-6xl font-semibold px-10 '
+                        {text}
+                    </span>
+                    <Cursor
+                        cursorColor='#C2D84B'
+                    />
+                </h1>
+                <div
+                    className='grid grid-cols-2 md:grid-cols-4 pt-10 gap-2 '
+                >
+                    <Link
+                        href="#about"
                     >
-                        <span
-                            className='mr-3'
+                        <button
+                            className='heroButton'
                         >
-                            {text}
-                        </span>
-                        <Cursor
-                            cursorColor='#C2D84B'
-                        />
-                    </h1>
-                    <div
-                        className='grid grid-cols-2 md:grid-cols-4 pt-5 gap-2 '
+                            About
+                        </button>
+                    </Link>
+                    <Link
+                        href="#experience"
                     >
-                        <Link
-                            href="#about"
+                        <button
+                            className='heroButton'
                         >
-                            <button
-                                className='heroButton'
-                            >
-                                About
-                            </button>
-                        </Link>
-                        <Link
-                            href="#experience"
+                            Experience
+                        </button>
+                    </Link>
+                    <Link
+                        href="#skills"
+                    >
+                        <button
+                            className='heroButton'
                         >
-                            <button
-                                className='heroButton'
-                            >
-                                Experience
-                            </button>
-                        </Link>
-                        <Link
-                            href="#skills"
+                            Skills
+                        </button>
+                    </Link>
+                    <Link
+                        href="#projects"
+                    >
+                        <button
+                            className='heroButton'
                         >
-                            <button
-                                className='heroButton'
-                            >
-                                Skills
-                            </button>
-                        </Link>
-                        <Link
-                            href="#projects"
-                        >
-                            <button
-                                className='heroButton'
-                            >
-                                Projects
-                            </button>
-                        </Link>
-                    </div>
+                            Projects
+                        </button>
+                    </Link>
                 </div>
             </div>
-        </Container>
+        </div>
+
     )
 }
 
